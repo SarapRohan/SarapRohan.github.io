8491b5ee1a784920e920f321f4d9896e1eef546e
Rohan Sarap <saraprohan@outlook.com>
2021-01-08 14:44:19 +0530

    Updates

--- src/components/layout.js
+++
@@ -1,6 +0,0 @@
-import "@fontsource/fira-code"
-import React from "react"
-import "./layout.scss"
-export default function Layout({ children }) {
-  return <div className="layout">{children}</div>
-}
